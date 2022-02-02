f3952032611671db84488c24798954799c2d2d02
Pamela Otéro <pamelasilvaoteroo@gmail.com>
2022-02-02 00:30:39 -0400

    style: ajuste da indentação da questão3

--- exercicio3.py
+++ exercicio3.py
@@ -3,20 +3,20 @@
 #   [a31, a32, a33] ]
 
 def verifica_identidade(matriz):
-	for linha in range(len(matriz)):
-		for coluna in range(len(matriz[linha])):
-			if (linha == coluna and matriz[linha][coluna] != 1):
-				return False
-			elif (linha != coluna and matriz[linha][coluna] != 0):
-				return False
-	return True
+    for linha in range(len(matriz)):
+        for coluna in range(len(matriz[linha])):
+            if (linha == coluna and matriz[linha][coluna] != 1):
+                return False
+            elif (linha != coluna and matriz[linha][coluna] != 0):
+                return False
+    return True
 
 # matriz = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
 
 if __name__=="__main__":
-	#print("Digite uma matriz: ")
-	matriz = eval(input())
-	if (verifica_identidade(matriz) == True):
-		print("sim")
-	else: 
-		print("nao")
+    #print("Digite uma matriz: ")
+    matriz = eval(input())
+    if (verifica_identidade(matriz) == True):
+        print("sim")
+    else: 
+        print("nao")
